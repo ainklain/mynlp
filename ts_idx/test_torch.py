9d2211d6e05f0ea73d1008ef39d1f2af32071cbe
KJH <ainklain@gmail.com>
2019-11-18 17:05:38 +0900

    torch maml

--- ts_idx/test_torch.py
+++ ts_idx/test_torch.py
@@ -27,7 +27,7 @@ def ex_dataloader():
     my_x = [np.array([[1.0,2],[3,4]]),np.array([[5.,6],[7,8]])] # a list of numpy arrays
     my_y = [np.array([4.]), np.array([2.])] # another list of numpy arrays (targets)
 
-    tensor_x = torch.stack([torch.Tensor(i) for i in my_x]) # transform to torch tensors
+    tensor_x = torch.stack([torch.Tensor(i) for i in my_x])  # transform to torch tensors
     tensor_y = torch.stack([torch.Tensor(i) for i in my_y])
 
     my_dataset = TensorDataset(tensor_x, tensor_y)  # create your datset
@@ -60,7 +60,7 @@ def arr_to_dataset(log_arr, sampling_freq=20):
     return arr_list, label_list
 
 
-def preprocessing(arr, image_size=64, to_dataloader=True, balancing=True, batch_size=1):
+def preprocessing(arr, image_size=64, to_dataloader=True, balancing=True, batch_size=1, classification=True):
     # Gramian Angular Field
     gasf = GramianAngularField(image_size=image_size, method='summation')
 
@@ -85,14 +85,20 @@ def preprocessing(arr, image_size=64, to_dataloader=True, balancing=True, batch_
 
     assert figs.shape[1:] == (3, image_size, image_size)
 
-    # 고정값
-    cp = 0.02
-    cp_l, cp_h = -cp, cp
-    # 변동값 (class수량 맞추기)
-    # cp_l, cp_h = np.percentile(label_arr, q=[33, 66])
-    # pos / zero / neg
-    label_class = np.stack([label_arr > cp_h, (label_arr >= cp_l) & (label_arr <= cp_h), label_arr < cp_l], axis=-1) * 1
-    print(np.sum(label_class, axis=0))
+
+    if classification:
+        # 고정값
+        cp = 0.02
+        cp_l, cp_h = -cp, cp
+        # 변동값 (class수량 맞추기)
+        # cp_l, cp_h = np.percentile(label_arr, q=[33, 66])
+        # pos / zero / neg
+        label_class = np.stack([label_arr > cp_h, (label_arr >= cp_l) & (label_arr <= cp_h), label_arr < cp_l], axis=-1) * 1
+        print(np.sum(label_class, axis=0))
+    else:
+        balancing = False
+        label_class = label_arr.reshape([-1, 1])
+
     d, l = figs.astype(np.float32), label_class.astype(np.float32)
 
     if balancing:
@@ -134,9 +140,8 @@ def preprocessing(arr, image_size=64, to_dataloader=True, balancing=True, batch_
         return (d, l)
 
 
-def make_dataloader(arr_y, split_r=[0.6, 0.8], batch_size=32):
+def make_dataloader(arr_y, split_r=[0.6, 0.8], batch_size=32, classification=True):
     # arr_y = np.array(data_df['kospi'], dtype=np.float32);split_r=[0.6, 0.8]; batch_size=32
-    arr_list = []
     arr_logp = np.log(np.cumprod(1 + arr_y) / (1 + arr_y[0]))
 
     train_arr = arr_logp[:int(len(arr_logp) * split_r[0])]
@@ -144,11 +149,109 @@ def make_dataloader(arr_y, split_r=[0.6, 0.8], batch_size=32):
     test_arr = arr_logp[int(len(arr_logp) * split_r[1]):]
 
     image_size = 64
-    dataloader_train, len_train = preprocessing(train_arr, image_size=image_size, to_dataloader=True, batch_size=batch_size)
-    dataloader_valid, len_valid = preprocessing(valid_arr, image_size=image_size, to_dataloader=True)
-    dataloader_test, len_test = preprocessing(test_arr, image_size=image_size, balancing=False, to_dataloader=True)
+    dataloader_train, len_train = preprocessing(train_arr, image_size=image_size, to_dataloader=True, batch_size=batch_size, classification=classification)
+    dataloader_valid, len_valid = preprocessing(valid_arr, image_size=image_size, to_dataloader=True, classification=classification)
+    dataloader_test, len_test = preprocessing(test_arr, image_size=image_size, balancing=False, to_dataloader=True, classification=classification)
 
     return dataloader_train, dataloader_valid, dataloader_test, [len_train, len_valid, len_test]
+
+
+def arr_to_dataset_ntasks(log_arr, sampling_freq=20):
+    m_days = 500
+    k_days = 20
+    delay_days = 1
+    k_days_adj = k_days + delay_days
+
+    arr_list, label_list = [], []
+    for i in range(m_days, len(log_arr) - (k_days_adj + 1), sampling_freq):
+        arr_list.append(log_arr[(i - m_days):(i + 1)])
+        label_list.append(log_arr[(i + delay_days):][k_days] - log_arr[(i + delay_days):][0])
+    return arr_list, label_list
+
+
+def convert_to_image(data_arr, image_size):
+    # Gramian Angular Field
+    gasf = GramianAngularField(image_size=image_size, method='summation')
+
+    # original
+    logp_scaled_spt = minmax_scaler(data_arr, axis=1)
+    fig1 = gasf.fit_transform(logp_scaled_spt)
+
+    # smoothing
+    logp_smoothed = data_arr[:, 60:] - data_arr[:, :-60]
+    logp_smoothed_scaled = minmax_scaler(logp_smoothed, axis=1)
+    fig2 = gasf.fit_transform(logp_smoothed_scaled)
+
+    # downsampling
+    logp_downsampled = data_arr[:, ::5]
+    logp_downsampled_scaled = minmax_scaler(logp_downsampled, axis=1)
+    fig3 = gasf.fit_transform(logp_downsampled_scaled)
+    figs = np.stack([fig1, fig2, fig3], axis=1)
+
+    assert figs.shape[1:] == (3, image_size, image_size)
+
+    return figs
+
+
+def preprocessing_ntasks(arr, image_size=64):
+
+    data_, label_ = arr_to_dataset_ntasks(arr, sampling_freq=5)
+    data_arr = np.stack(data_, axis=0)
+    label_arr = np.stack(label_, axis=0)
+
+    n_classes = 2
+    label_class = ((label_arr < 0) * 1).astype(np.float32)  # 0보다 크면 0, 작으면 1
+
+    figs = convert_to_image(data_arr, image_size).astype(np.float32)
+
+    transformer = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()])
+
+    figs = torch.stack([transformer(torch.from_numpy(fig)) for fig in figs]).numpy()
+
+    n_spt = 24  # n_spt * sampling_freq = n_days for support
+
+    k_shot = 5
+    d_spt_list, l_spt_list, d_qry_list, l_qry_list = [], [], [], []
+    for i in range(n_spt, len(figs)):
+        selected = label_class[(i - n_spt):i]
+        idx = np.arange(len(selected))
+
+        random_sampled = []
+        for class_i in range(n_classes):
+            idx_c = np.random.choice(idx[selected == class_i], k_shot, replace=True)
+            random_sampled.append(idx_c)
+        idx_balanced = np.random.permutation(np.concatenate(random_sampled))
+        d_spt_list.append(figs[(i - n_spt):i][idx_balanced])
+        l_spt_list.append(label_class[(i - n_spt):i][idx_balanced])
+        d_qry_list.append(figs[i:(i+1)])
+        l_qry_list.append(label_class[i:(i+1)])
+
+    spt_x = torch.from_numpy(np.stack(d_spt_list))  # transform to torch tensors
+    spt_y = torch.from_numpy(np.stack(l_spt_list))
+    qry_x = torch.from_numpy(np.stack(d_qry_list))  # transform to torch tensors
+    qry_y = torch.from_numpy(np.stack(l_qry_list))
+
+    return (spt_x, spt_y, qry_x, qry_y)
+
+
+def make_dataloader_ntasks(arr_y, split_r=[0.6, 0.8]):
+    # arr_y = np.array(data_df['kospi'], dtype=np.float32);split_r=[0.6, 0.8]; batch_size=32
+    arr_logp = np.log(np.cumprod(1 + arr_y) / (1 + arr_y[0]))
+
+    train_arr = arr_logp[:int(len(arr_logp) * split_r[0])]
+    valid_arr = arr_logp[int(len(arr_logp) * split_r[0]):int(len(arr_logp) * split_r[1])]
+    test_arr = arr_logp[int(len(arr_logp) * split_r[1]):]
+
+    image_size = 64
+    dataset_train = preprocessing_ntasks(train_arr, image_size=image_size)
+    dataset_valid = preprocessing_ntasks(valid_arr, image_size=image_size)
+    dataset_test = preprocessing_ntasks(test_arr, image_size=image_size)
+
+    return dataset_train, dataset_valid, dataset_test
+
 
 
 def np_to_tensor(list_of_numpy_objs):
@@ -272,6 +375,7 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, dataset_siz
     return model
 
 
+# one task
 def train_maml(model, criterion, optimizer, scheduler, dataloaders, dataset_sizes, lr_inner=0.01, num_epochs=25):
     # num_epochs=25;phase='train';lr_inner=0.01
     since = time.time()
@@ -308,7 +412,7 @@ def train_maml(model, criterion, optimizer, scheduler, dataloaders, dataset_size
                 # 순전파
                 # 학습 시에만 연산 기록을 추적
                 outputs = model.forward(inputs)
-                outputs = F.softmax(outputs)
+                # outputs = F.softmax(outputs)
                 _, preds = torch.max(outputs, 1)
                 train_loss = criterion(outputs, labels)
 
@@ -377,7 +481,7 @@ def train_maml(model, criterion, optimizer, scheduler, dataloaders, dataset_size
             # 학습 시에만 연산 기록을 추적
             with torch.no_grad():
                 outputs = model(inputs)
-                outputs = F.softmax(outputs)
+                # outputs = F.softmax(outputs)
                 _, preds = torch.max(outputs, 1)
                 loss = criterion(outputs, labels)
 
@@ -392,9 +496,105 @@ def train_maml(model, criterion, optimizer, scheduler, dataloaders, dataset_size
 
     return model
 
+# many tasks
+def train_maml2(model, criterion, optimizer, scheduler, dataloaders, lr_inner=0.01, num_epochs=25):
+    # num_epochs=25;phase='train';lr_inner=0.01
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # 각 에폭(epoch)은 학습 단계와 검증 단계를 갖습니다.
+        for phase in ['train', 'valid']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()  # 모델을 학습 모드로 설정
+            else:
+                model.eval()   # 모델을 평가 모드로 설정
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            n_tasks = 20
+            tasks_idxs = np.random.choice(len(dataloaders), n_tasks, replace=True)
+            task_losses = None
+
+            x_spt, y_spt, x_qry, y_qry = dataloaders[phase]
+            for i, task_i in enumerate(tasks_idxs):
+                x_spt_i, y_spt_i, x_qry_i, y_qry_i = x_spt[task_i], y_spt[task_i], x_qry[task_i], y_qry[task_i]
+                # print('after balancing: {}'.format(torch.sum(labels_balanced, axis=0)))
+                inputs_spt = x_spt_i.to(device)
+                labels_spt = y_spt_i.long().to(device)
+                inputs_qry = x_qry_i.to(device)
+                labels_qry = y_qry_i.long().to(device)
+
+                # # 매개변수 경사도를 0으로 설정
+                # optimizer.zero_grad()
+
+                # 순전파
+                # 학습 시에만 연산 기록을 추적
+                outputs_spt = model.forward(inputs_spt)
+                # outputs = F.softmax(outputs)
+                _, preds = torch.max(outputs_spt, 1)
+                train_loss = criterion(outputs_spt, labels_spt)
+                # Step 6
+                grad = torch.autograd.grad(train_loss, model.parameters(), retain_graph=True, create_graph=True)
+
+                fast_weights = list(map(lambda p: p[1] - lr_inner * p[0], zip(grad, model.parameters())))
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs_qry = model.forward(inputs_qry, fast_weights)  # run forward pass to initialize weights
+                    test_loss = criterion(outputs_qry, labels_qry)
+                    if task_losses is None:
+                        task_losses = test_loss
+                    else:
+                        task_losses += test_loss
+
+                with torch.no_grad():
+                    # 통계
+                    running_loss += test_loss.item() * inputs_qry.size(0)
+                    # running_corrects += torch.sum(preds == labels.data)
+                    running_corrects += torch.sum(torch.max(outputs_qry, 1)[1] == labels_qry.data)
+
+            # 학습 단계인 경우 역전파 + 최적화
+            if phase == 'train':
+                total_losses = task_losses / n_tasks
+                optimizer.zero_grad()
+                total_losses.backward()
+                optimizer.step()
+
+            epoch_loss = running_loss / n_tasks
+            epoch_acc = running_corrects.double() / n_tasks
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # 모델을 깊은 복사(deep copy)함
+            if phase == 'valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+
+    # 가장 나은 모델 가중치를 불러옴
+    model.load_state_dict(best_model_wts)
+
+
+    return model
+
 
 class ImageModel(nn.Module):
-    def __init__(self, base_model='resnet50', configs=None):
+    def __init__(self, output_size, base_model='resnet50', configs=None):
         super().__init__()
         if base_model == 'resnet50':
             base_model = models.resnet50(pretrained=True)
@@ -412,7 +612,7 @@ class ImageModel(nn.Module):
                               ('relu', []),
                               ('linear', [16, 32]),
                               ('relu', []),
-                              ('linear', [3, 16])]
+                              ('linear', [output_size, 16])]
         else:
             self.structure = configs
         for name, param in self.structure:
@@ -486,14 +686,20 @@ def main():
     # arr_y = np.array(data_df['mkt_rf'], dtype=np.float32)
     arr_y = np.array(data_df['mom'], dtype=np.float32)
 
+    classification = False
+    if classification:
+        output_size = 3
+    else:
+        output_size = 1
+
     # make dataloader
-    dl_train, dl_valid, dl_test, ds_sizes = make_dataloader(arr_y, batch_size=32)
+    dl_train, dl_valid, dl_test, ds_sizes = make_dataloader(arr_y, batch_size=32, classification=classification)
     dataloaders = {'train': dl_train, 'valid': dl_valid, 'test': dl_test}
     dataset_sizes = {'train': ds_sizes[0], 'valid': ds_sizes[1], 'test': ds_sizes[2]}
 
     model_ft = models.resnet50(pretrained=True)
     num_ftrs = model_ft.fc.in_features
-    model_ft.fc = nn.Linear(num_ftrs, 3)
+    model_ft.fc = nn.Linear(num_ftrs, output_size)
     model_ft = model_ft.to(device)
 
     criterion = nn.MSELoss()
@@ -512,7 +718,7 @@ def main():
 
     # 새로 생성된 모듈의 매개변수는 기본값이 requires_grad=True 임
     num_ftrs = model_conv.fc.in_features
-    model_conv.fc = nn.Linear(num_ftrs, 3)
+    model_conv.fc = nn.Linear(num_ftrs, output_size)
 
     model_conv = model_conv.to(device)
 
@@ -536,13 +742,19 @@ def main_maml():
     # arr_y = np.array(data_df['mkt_rf'], dtype=np.float32)
     arr_y = np.array(data_df['mom'], dtype=np.float32)
 
+    classification = False
+    if classification:
+        output_size = 3
+    else:
+        output_size = 1
+
     # make dataloader
-    dl_train, dl_valid, dl_test, ds_sizes = make_dataloader(arr_y, batch_size=32)
+    dl_train, dl_valid, dl_test, ds_sizes = make_dataloader(arr_y, batch_size=32, classification=classification)
     dataloaders = {'train': dl_train, 'valid': dl_valid, 'test': dl_test}
     dataset_sizes = {'train': ds_sizes[0], 'valid': ds_sizes[1], 'test': ds_sizes[2]}
 
 
-    model = ImageModel(base_model='resnet50')
+    model = ImageModel(output_size, base_model='resnet50')
     model = model.to(device)
 
     criterion = nn.MSELoss()
@@ -554,5 +766,40 @@ def main_maml():
     scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
     model_conv = train_maml(model, criterion, optimizer, scheduler,
                              dataloaders, dataset_sizes, lr_inner=0.01, num_epochs=25)
+
+
+
+
+def main_maml2():
+
+    # get dataset
+    data_df = prepare_dataset()
+
+    # arr_y = np.array(data_df['mkt_rf'], dtype=np.float32)
+    arr_y = np.array(data_df['mom'], dtype=np.float32)
+
+    classification = True
+    if classification:
+        output_size = 2
+    else:
+        output_size = 1
+
+    # make dataloader
+    dl_train, dl_valid, dl_test = make_dataloader_ntasks(arr_y)
+    dataloaders = {'train': dl_train, 'valid': dl_valid, 'test': dl_test}
+
+
+    model = ImageModel(output_size, base_model='resnet50')
+    model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # 이전과는 다르게 마지막 계층의 매개변수들만 최적화되는지 관찰
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # 7 에폭마다 0.1씩 학습율 감소
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+    model_conv = train_maml2(model, criterion, optimizer, scheduler,
+                             dataloaders, lr_inner=0.01, num_epochs=100)
 
 
--- ts_mini/data_process_v2_1_mini.py
+++ ts_mini/data_process_v2_1_mini.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 import pickle
-import tensorflow as tf
+# import tensorflow as tf
 import time
 import numpy as np
 import os
